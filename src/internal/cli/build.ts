a6596170d63005ec4b106654b8b09b69164da894
Thada Wangthammang <thada.wth@gmail.com>
2024-07-10 20:16:12 +0700

    chore: refactor build for handling MatchRunner

--- src/internal/cli/build.ts
+++ src/internal/cli/build.ts
@@ -1,50 +1,9 @@
-import glob from "tiny-glob";
 import { logger } from "../libs/logger.js";
-import path from "node:path";
-import { MatchRunner } from "../match-runner.js";
-import { envSchema } from "../env.schema.js";
+import { executeMatchRunner, type BaseCommandOptions } from "./helper.js";
 
-export interface LabelResourcesOptions {
-  testGlobPattern: string;
-}
-
-function validateMatchFile(matchImport: unknown, file: string): matchImport is { default: MatchRunner } {
-  if (!matchImport) {
-    logger.warn(`No export found in '${file}'`);
-    return false;
-  }
-  if (typeof matchImport !== "object") {
-    logger.warn(`Export in '${file}' is not an object`);
-    return false;
-  }
-  if (!("default" in matchImport)) {
-    logger.warn(`No default export found in '${file}'`);
-    return false;
-  } else if (matchImport.default instanceof MatchRunner) {
-    return true;
-  }
-  logger.warn(`Default export in '${file}' is not a MatchRunner`);
-  return false;
-}
-
-export async function build(options: LabelResourcesOptions) {
-  const { testGlobPattern } = options;
-  const env = envSchema.parse(process.env);
-  logger.info(`Running with environment: ${JSON.stringify(env)}`);
-  logger.info(`Test Match Pattern: '${testGlobPattern}'`);
-  logger.log("Labeling resources...");
-
-  const files = await glob(testGlobPattern);
-  console.log(files);
-  for (const file of files) {
-    const match = (await import(path.resolve(file))) as unknown;
-    if (!validateMatchFile(match, file)) {
-      continue;
-    }
-    const matchRunner = match.default;
-    matchRunner.init({
-      debug: env.DEBUG,
-    });
-    await matchRunner.labelResources();
-  }
+export async function build(options: BaseCommandOptions) {
+  await executeMatchRunner(options, async instance => {
+    logger.log("Building resources...");
+    await instance.labelResources();
+  });
 }
---
+++ src/internal/cli/helper.ts
@@ -0,0 +1,54 @@
+import glob from "tiny-glob";
+import { envSchema } from "../env.schema.js";
+import { logger } from "../libs/logger.js";
+import { MatchRunner } from "../match-runner.js";
+import path from "node:path";
+
+export type MaybePromise<T> = T | Promise<T>;
+
+export interface BaseCommandOptions {
+  testGlobPattern: string;
+}
+
+export function validateMatchFile(matchImport: unknown, file: string): matchImport is { default: MatchRunner } {
+  if (!matchImport) {
+    logger.warn(`No export found in '${file}'`);
+    return false;
+  }
+  if (typeof matchImport !== "object") {
+    logger.warn(`Export in '${file}' is not an object`);
+    return false;
+  }
+  if (!("default" in matchImport)) {
+    logger.warn(`No default export found in '${file}'`);
+    return false;
+  } else if (matchImport.default instanceof MatchRunner) {
+    return true;
+  }
+  logger.warn(`Default export in '${file}' is not a MatchRunner`);
+  return false;
+}
+
+export async function executeMatchRunner(
+  options: BaseCommandOptions,
+  fn: (instance: MatchRunner) => MaybePromise<void>
+): Promise<void> {
+  const { testGlobPattern } = options;
+  const env = envSchema.parse(process.env);
+  logger.info(`Running with environment: ${JSON.stringify(env)}`);
+  logger.info(`Test Match Pattern: '${testGlobPattern}'`);
+
+  const files = await glob(testGlobPattern);
+  console.log(files);
+  for (const file of files) {
+    const match = (await import(path.resolve(file))) as unknown;
+    if (!validateMatchFile(match, file)) {
+      continue;
+    }
+    const matchRunner = match.default;
+    matchRunner.init({
+      debug: env.DEBUG,
+    });
+    await fn(matchRunner);
+  }
+}
